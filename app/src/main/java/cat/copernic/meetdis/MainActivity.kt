9d988322c7f0c5adf93c0c254bb50fb71b7a17a7
Marc Hernandez <hernandez.salazar.marc@alumnat.copernic.cat>
2021-10-28 05:32:19 +0200

    Creación Log In

--- app/src/main/java/cat/copernic/meetdis/MainActivity.kt
+++ app/src/main/java/cat/copernic/meetdis/MainActivity.kt
@@ -2,10 +2,28 @@ package cat.copernic.meetdis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val boton: Button = findViewById(R.id.b_entra)
+        var holamon: TextView = findViewById(R.id.hello)
+        var nom:String = "Holaaa"
+
+        boton.setOnClickListener{cambio()}
+
+    }
+
+    fun cambio() {
+        val boton: Button = findViewById(R.id.b_entra)
+        var holamon: TextView = findViewById(R.id.hello)
+        var nom:String = "XD"
+
+        holamon.text = nom
+
+
     }
 }
