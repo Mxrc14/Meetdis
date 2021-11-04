61a992ad2631313b14f3824f47faac6057531b42
Adria Tomas <tomas.morancho.adria@alumnat.copernic.cat>
2021-11-04 10:03:07 +0100

    navigation i pantalla animació

--- app/src/main/java/cat/copernic/meetdis/Contacte.kt
+++ app/src/main/java/cat/copernic/meetdis/Contacte.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Notificacio.newInstance] factory method to
+ * Use the [Contacte.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Notificacio : Fragment() {
+class Contacte : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class Notificacio : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notificacio, container, false)
+        return inflater.inflate(R.layout.fragment_contacte, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class Notificacio : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Notificacio.
+         * @return A new instance of fragment Contacte.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Notificacio().apply {
+            Contacte().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/cat/copernic/meetdis/MainActivity.kt
+++ app/src/main/java/cat/copernic/meetdis/MainActivity.kt
@@ -2,29 +2,30 @@ package cat.copernic.meetdis
 
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import cat.copernic.meetdis.R
 import cat.copernic.meetdis.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+       Thread.sleep(3000)
+        setTheme(R.style.AppTheme)
         @Suppress("UNUSED_VARIABLE")
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         drawerLayout = binding.drawerLayout
-
-        val navController = this.findNavController(R.id.mainActivityFragment)
-
-        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
-
-        NavigationUI.setupWithNavController(binding.navView, navController)
 
     }
 
