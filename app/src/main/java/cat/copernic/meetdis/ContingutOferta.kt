a84a9d9e30c3f1cff3ba0ba5ceb3628106a62e50
Marc Hernandez <hernandez.salazar.marc@alumnat.copernic.cat>
2022-01-13 00:16:31 +0100

    Notificacio

--- app/src/main/java/cat/copernic/meetdis/ContingutOferta.kt
+++ app/src/main/java/cat/copernic/meetdis/ContingutOferta.kt
@@ -6,6 +6,8 @@ import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import cat.copernic.meetdis.databinding.FragmentContingutOfertaBinding
+import android.content.Intent
+import cat.copernic.meetdis.MainActivity as MainActivity1
 
 
 class ContingutOferta : Fragment() {
@@ -20,6 +22,21 @@ class ContingutOferta : Fragment() {
             inflater,
             R.layout.fragment_contingut_oferta, container, false
         )
+
+        val intent: Intent = requireActivity().getIntent()
+        val bundle = intent.extras
+
+        var titol = bundle!!.getSerializable("ofertaTitol")
+        var descripcio = bundle!!.getSerializable("ofertaDesc")
+        var data = bundle!!.getSerializable("ofertaData")
+        var dni = bundle!!.getSerializable("ofertaDNI")
+        var img = bundle!!.getSerializable("ofertaImg")
+        var lat = bundle!!.getSerializable("ofertaLat")
+        var lon = bundle!!.getSerializable("ofertaLon")
+
+
+
+
 
         //val args = BuscarArgs.fromBundle(requireArguments())
 
