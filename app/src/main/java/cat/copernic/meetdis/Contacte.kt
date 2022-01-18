ea8987218e5108245d895c4a54063ad58d1dd05f
Adria Tomas <tomas.morancho.adria@alumnat.copernic.cat>
2022-01-19 00:03:48 +0100

    SegundoViewModelHecho

--- app/src/main/java/cat/copernic/meetdis/Contacte.kt
+++ app/src/main/java/cat/copernic/meetdis/Contacte.kt
@@ -5,55 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import cat.copernic.meetdis.databinding.FragmentAboutBinding
+import cat.copernic.meetdis.databinding.FragmentContacteBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Contacte.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Contacte : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentContacteBinding
+
+    private lateinit var viewModel: ContacteViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacte, container, false)
+
+        binding = FragmentContacteBinding.inflate(layoutInflater)
+
+
+        viewModel = ViewModelProvider(this).get(ContacteViewModel::class.java)
+
+
+
+       viewModel.nomICognom.observe(viewLifecycleOwner, {
+            binding.textDNI.hint = it.toString()
+        })
+       viewModel.titol.observe(viewLifecycleOwner, {
+            binding.textTitol.hint = it.toString()
+        })
+        viewModel.descripcio.observe(viewLifecycleOwner, {
+            binding.descripcio.hint = it.toString()
+        })
+        viewModel.enviar.observe(viewLifecycleOwner, {
+            binding.bEnviar.text = it.toString()
+        })
+
+
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Contacte.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Contacte().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
+
+
 }
