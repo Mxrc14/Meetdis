f0b338cc76eb029d79e435950bf8150d8f4bd7b4
Adria Tomas <tomas.morancho.adria@alumnat.copernic.cat>
2022-01-23 23:57:57 +0100

    Validació usuaris

--- app/src/main/java/cat/copernic/meetdis/ValidacioUsuaris.kt
+++ app/src/main/java/cat/copernic/meetdis/ValidacioUsuaris.kt
@@ -6,21 +6,73 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import cat.copernic.meetdis.databinding.FragmentContingutOfertaBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import cat.copernic.meetdis.adapters.MembreRecyclerAdapter
+import cat.copernic.meetdis.adapters.ValidacioRecyclerAdapter
+import cat.copernic.meetdis.databinding.FragmentMembresBinding
+import cat.copernic.meetdis.databinding.FragmentValidacioUsuarisBinding
+import cat.copernic.meetdis.models.Busca
+import cat.copernic.meetdis.models.Membre
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ValidacioUsuaris : Fragment() {
+
+    private var myAdapter: ValidacioRecyclerAdapter = ValidacioRecyclerAdapter()
+
+    private val db = FirebaseFirestore.getInstance()
+
+
+    private var membres: ArrayList<Membre> = arrayListOf();
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentContingutOfertaBinding>(
+
+
+        val binding = DataBindingUtil.inflate<FragmentValidacioUsuarisBinding>(
             inflater,
-            R.layout.fragment_contingut_oferta, container, false
+            R.layout.fragment_validacio_usuaris, container, false
         )
 
-        return binding.root
-    }
+
+        binding.rvValidacion.setHasFixedSize(true)
+
+        binding.rvValidacion.layoutManager = LinearLayoutManager(requireContext())
 
 
-}
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { documents ->
+                membres.clear()
+                for (document in documents) {
+                    membres.add(
+                        Membre(
+                            document.get("cognoms").toString(),
+                            document.get("contrasenya").toString(),
+                            document.get("correu").toString(),
+                            document.get("nom").toString(),
+                            document.get("tipus d´usuari").toString(),
+                            document.id
+                        )
+                    )
+                }
+                context?.let { myAdapter.ValidacioRecyclerAdapter(membres, it) }
+                binding.rvValidacion.adapter = myAdapter
+
+            }
+
+
+            return binding.root
+                    }
+
+
+            }
+
+
+
+
+
+
---
+++ app/src/main/java/cat/copernic/meetdis/adapters/ValidacioRecyclerAdapter.kt
@@ -0,0 +1,79 @@
+package cat.copernic.meetdis.adapters
+
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+import androidx.recyclerview.widget.RecyclerView
+import cat.copernic.meetdis.databinding.ItemMembreListBinding
+import cat.copernic.meetdis.models.Membre
+import com.google.firebase.storage.FirebaseStorage
+import kotlin.collections.ArrayList
+import coil.api.load
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ValidacioRecyclerAdapter: RecyclerView.Adapter<ValidacioRecyclerAdapter.ViewHolder>(){
+
+
+    var membres: ArrayList<Membre> = ArrayList()
+    lateinit var context: Context
+    lateinit var id: String
+
+    private val db = FirebaseFirestore.getInstance()
+
+    fun ValidacioRecyclerAdapter(membresList: ArrayList<Membre>, contxt: Context) {
+        this.membres = membresList
+        this.context = contxt
+    }
+
+    //és l'encarregat de retornar el ViewHolder ja configurat
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(
+            ItemMembreListBinding.inflate(layoutInflater, parent, false)
+        )
+    }
+
+    //Aquest mètode s'encarrega de passar els objectes, un a un al ViewHolder personalitzat
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        with(holder) {
+            with(membres[position]) {
+                binding.txtNom.text = this.nomMembre
+
+                var imatge = this.imageMembre
+                //Monstrar la imatge des de Storage de Firebase
+                val storageRef = FirebaseStorage.getInstance().reference
+
+                this.imageMembre?.let { Log.i("cash", it) }
+
+                val imageRef = storageRef.child("users/$imageMembre")
+                imageRef.downloadUrl.addOnSuccessListener { url ->
+                    binding.imgMembre.load(url)
+
+                }.addOnFailureListener {
+                    Log.i("cash", "Falla")
+                }
+            }
+        }
+
+        val item = membres[position]
+
+        holder.bind(item)
+    }
+
+
+    override fun getItemCount(): Int {
+        return membres.size
+    }
+
+
+    class ViewHolder(val binding: ItemMembreListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Membre) {
+
+        }
+    }
+}
